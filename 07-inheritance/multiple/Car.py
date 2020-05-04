98b6e33c66af3ab2f1a49cdd0d3ba1f3dd514e92
John Hunt <jeh@midmarsh.co.uk>
2020-05-04 21:05:30 +0100

    Inheritance Examples

---
+++ 07-inheritance/multiple/Car.py
@@ -0,0 +1,27 @@
+class Car:
+    """ Car """
+
+    def move(self):
+        print('Car - move()')
+
+
+class Toy:
+    """ Toy """
+
+    def move(self):
+        print('Toy - move()')
+
+
+class ToyCar(Car, Toy):
+    """ A Toy Car """
+
+class CarToy(Toy, Car):
+    """ A Toy Car """
+
+tc = ToyCar()
+tc.move()
+
+print('-' * 30)
+
+ct = CarToy()
+ct.move()
