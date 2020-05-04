76fcc2c956064ea467bd897de3fc6f859828b982
John Hunt <jeh@midmarsh.co.uk>
2020-05-04 14:09:38 +0100

    Refactored to align with current course outline

--- 10-protocols/Calculator.py
+++ 10-protocols/Calculator.py
@@ -32,7 +32,7 @@ calc = Calculator()
 
 print('calc.add(3, 4):', calc.add(3, 4))
 print('calc.add(3, 4.05-builtin-higher-order):', calc.add(3, 4.5))
-print('calc.add(4.05-builtin-higher-order, 6.2):', calc.add(4.5, 6.2))
+print('calc.add(4.05-builtin-higher-order, 06-higherorderfunctions.2):', calc.add(4.5, 6.2))
 print('calc.add(2.3, 7):', calc.add(2.3, 7))
 print('calc.add(-1, 4):', calc.add(-1, 4))
 print("calc.add('John', 'Hunt')", calc.add('John', 'Hunt'))
